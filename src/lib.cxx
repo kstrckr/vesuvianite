dc8324112e38fea1de4d7f49e945534d8f3e6a77
Kurt Strecker <kurt@kurtstrecker.io>
2021-12-31 00:28:42 -0500

    lib succesfully using opencv

--- src/lib.cxx
+++ src/lib.cxx
@@ -1,10 +1,21 @@
-#include <iostream>
+#include <stdio.h>
+#include <opencv4/opencv2/opencv.hpp>
 #include <vesuvianite/lib.hpp>
 using namespace std;
 
 namespace lib {
   int loadRaw() {
-    cout << "Hello, World from vesuvianite lib!" << endl; // This prints Hello, World!
+
+    cv::Mat image;
+    image = cv::imread( "/home/a/proj/vesuvianite/ideal-target-batch-1/51A_8_verso.cr2" );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+    cv::waitKey(0);
     return 0;
   }
 } // namespace lib
